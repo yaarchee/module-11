e2f976f4600beb0c99f3364cb7cb7fa5e0bdda12
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-05-23 14:52:29 +0300

    some fix

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,19 +3,22 @@ let menuToggle = document.querySelector('.menuToggle');
 let body = document.querySelector('body');
 let navListWrap = document.querySelector('.nav-list-wrap');
 let overlayPage = document.querySelector('.overlay-page');
+let links = document.querySelectorAll('.nav-list__link');
 
 
-menuToggle.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => toggleClass());
+
+overlayPage.addEventListener('click', () => toggleClass());
+
+for( let link of links){
+  link.addEventListener('click', () => toggleClass());
+}
+
+
+
+function toggleClass() {
   body.classList.toggle('none-scroll');
   navListWrap.classList.toggle('selected');
   overlayPage.classList.toggle('on-overlay');
   menuToggle.classList.toggle('on-hidden');
-});
-
-overlayPage.addEventListener('click', () => {
-  body.classList.remove('none-scroll');
-  navListWrap.classList.remove('selected');
-  menuToggle.classList.remove('on-hidden');
-  overlayPage.classList.remove('on-overlay');
-});
-
+}
